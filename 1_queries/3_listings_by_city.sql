df4d06da1616265476a6aaff51ec41ccda35e85a
Micheala Palmer <michealapalmer@gmail.com>
2022-01-05 22:59:41 -0500

    create queries based on requirements

---
+++ 1_queries/3_listings_by_city.sql
@@ -0,0 +1,7 @@
+SELECT properties.*, AVG(property_reviews.rating) FROM properties
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
---
+++ 1_queries/5_user_reservation_view.sql
@@ -0,0 +1,7 @@
+SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating FROM reservations
+JOIN properties ON properties.id = reservations.property_id 
+JOIN property_reviews ON property_reviews.reservation_id = reservations.id 
+WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date
+LIMIT 10;
