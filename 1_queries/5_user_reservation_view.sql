0d400d6370f644eb9cd545a20b196d8fc94ebc71
Micheala Palmer <michealapalmer@gmail.com>
2022-01-06 19:33:21 -0500

    update reservations query and connect to database

--- 1_queries/5_user_reservation_view.sql
+++ 1_queries/5_user_reservation_view.sql
@@ -2,6 +2,6 @@ SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rat
 JOIN properties ON properties.id = reservations.property_id 
 JOIN property_reviews ON property_reviews.reservation_id = reservations.id 
 WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
-GROUP BY reservations.id, properties.id
+GROUP BY reservations.id, properties.id, users.email
 ORDER BY reservations.start_date
 LIMIT 10;
